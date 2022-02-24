74470076061d6782ef527f3c160d62e478e36a9b
littlej77 <jordantempest@gmail.com>
2022-02-23 19:47:57 -0700

    fixed the page num

--- Infastructure/PaginationTagHelper.cs
+++ Infastructure/PaginationTagHelper.cs
@@ -44,7 +44,7 @@ namespace BookstoreProj.Infastructure
             IUrlHelper uh = uhf.GetUrlHelper(vc);
             TagBuilder final = new TagBuilder("div");
 
-            for (int i = 1; i <= (PageBlah.TotalPages +1); i++)
+            for (int i = 1; i <= (PageBlah.TotalPages); i++)
             {
                 TagBuilder tb = new TagBuilder("a");
 
